531a2f0f74b89fcb0381c1233d770c3a16b6235b
Sage Brown <sage.brown@gmail.com>
2017-06-21 11:25:00 -0700

    JS cleanup + commenting

--- js/sage.js
+++ js/sage.js
@@ -1,74 +1,32 @@
 // sagebrown.com
 // 2017
 
+// VARIABLES
+
       var currentImage = 0;
-			var numImages = $(".row").children().length;
+      //array of #title, images, and .next-up objects in gallery
       var pageImages = $(".row").children();
-      var containerLeft;
+
+      // total number of objects
+      var numImages = pageImages.length;
+
+      //var containerLeft;
+
+      // image margin in gallery, equal to CSS value
 			var imgMargin = 50;
+
+      // keeps track of browser window width
       var viewportWidth;
+
+      // quasi arbitrary mobile breakpoint size
       var mobileBreak = 800;
 
-			//$(document).ready(setWidth());
-			$(window).on('load', function() {
-				setWidth();
+// LISTENERS
+$(document).ready(function() {
+    $(window).on('resize scroll load', pageInit).trigger('scroll');
+});
 
-      $(".title").css("line-height", $(".title").height() + "px");
-				//jQuery.easing.def = "easeOutQuad";
-    if(viewportWidth < mobileBreak) {
-
-        $(".title").each(function() {
-          var lineHeight = $(this).parent().children("img:first-child").height()
-          $(this).css("line-height", lineHeight + "px");
-          console.log(lineHeight)
-        });
-        }
-        $(".secondary-thumb").hide();
-			});
-
-			$(window).on('resize', function() {
-				setWidth();
-        $(".title").css("line-height", $(".title").height() + "px");
-
-        if(viewportWidth < mobileBreak) {
-
-            $(".title").each(function() {
-              var lineHeight = $(this).parent().children("img:first-child").height()
-              $(this).css("line-height", lineHeight + "px");
-              console.log(lineHeight)
-            });
-            }
-			});
-
-			function setWidth() {
-        viewportWidth = window.outerWidth;
-
-        if($(".row").length > 0) {
-  				var totalWidth = 0;
-  				$(".row").children().each(function() {
-  					totalWidth += $(this).width();
-  				});
-
-  			  	$(".row").width(totalWidth + (numImages*imgMargin));
-  			  	console.log($(".row").width());
-  			  	rowX = $(".row").position().left;
-
-  			  	//set title size
-  			  	$("#title").width($(".row img").height()*.8);
-
-          //set container left position
-            containerLeft = $("#container").position().left;
-            console.log("setWidth");
-          }
-		  	}
-
-			$("#getPos").click(function() {
-				navWidth = $("#nav").outerWidth();
-				console.log(navWidth);
-
-			});
-
-
+      // listen for keypress commands to advance images left / right
       $("body").keydown(function(e) {
         if(e.keyCode == 37) { // left
           if(currentImage > 0) {
@@ -84,101 +42,149 @@
         }
       });
 
+
+// FUNCTIONS
+
+      // size, position, adjust page items
+			function pageInit() {
+        // hide the secondary-thumb elements
+        $(".secondary-thumb").hide(); // hide secondary thumbnails for rotations
+
+        // set viewportWidth variable to window outerWidth
+        viewportWidth = window.outerWidth;
+
+        // check to see if we're below the mobile breakpoint size
+        if(viewportWidth < mobileBreak) {
+          // if so, adjust the line height of homepage grid titles for mobile
+          $(".title").each(function() {
+            var lineHeight = $(this).parent().children("img:first-child").height()
+            $(this).css("line-height", lineHeight + "px");
+          });
+        } else {
+          // if we're not on mobile, set them to the height of the grid element
+          $(".title").css("line-height", $(".title").height() + "px");
+        }
+
+        // adjust the .row width based on number of images in gallery
+        // first check to see if .row exists
+        if($(".row").length > 0) {
+  				var totalWidth = 0;
+
+          // loop through childre (images) in row and add width
+          // this accounts for variable width of horizontal / vertical images
+  				$(".row").children().each(function() {
+  					totalWidth += $(this).width();
+  				});
+
+          // set .row width and add in extra space for the image margins
+			  	$(".row").width(totalWidth + (numImages*imgMargin));
+
+          // i think this is old, not sure what it's for
+			  	// rowX = $(".row").position().left;
+
+  			  	// set gallery title card size, based on image height
+  			  	$("#title").width($(".row img").height()*.8);
+
+          // set container left position, used for next / previous scrolling
+          //containerLeft = $("#container").position().left;
+          }
+		  	}
+
+        // handles rotating through images on homepage grid
+        $.fn.rotate = function(){
+          return this.each(function() {
+
+            /* Cache element's children */
+            var $children = $(this).children("img");
+
+            /* Current element to display */
+            var position = -1;
+
+            /* IIFE */
+            !function loop() {
+
+                /* Get next element's position.
+                 * Restarting from first children after the last one.
+                 */
+                position = (position + 1) % $children.length;
+
+                /* Fade element */
+                $children.eq(position).fadeIn(0).delay(300).fadeOut(0, loop);
+            }();
+          });
+
+          // scrolls to a specific image
+          function scrollTo(nextImg) {
+            if(currentImage > 0) {
+              // if we're past the beginning this sets the previous cursor to an arrow
+              $("#previous").css("cursor", "w-resize")
+            } else {
+              // if we're at the start, #previous cursor is set to default
+              $("#previous").css("cursor", "default")
+            }
+
+            if(currentImage == numImages-2) {
+              // if we're at the end, #next cursor is set to default
+              $("#next").css("cursor", "default")
+            } else {
+              // otherwise, it's an arrow
+              $("#next").css("cursor", "e-resize")
+            }
+
+            // this is the main scroll action
+
+            $("#container").animate({
+              // gets the nextImg left position and scrolls the container to there
+              scrollLeft: $(pageImages[nextImg]).position().left
+            }, 500, function(){
+              //do something after if needed
+            });
+          };
+
+// CLICK EVENTS
+
+      // next image object
       $("#next").click(function(){
+        // checks to make sure we're not at the last image
         if(currentImage < numImages-2) {
+          // adds to the currentImage variable
           currentImage++;
+          // calls the function to scroll to the next image
           scrollTo(currentImage)
         }
       });
 
       $("#previous").click(function(){
+        // checks to make sure we're beyond the start so we don't go backwards
         if(currentImage > 0) {
+          // subtracts from the currentImage
           currentImage--;
+          // calls the function to scroll to the variable
           scrollTo(currentImage)
         }
       });
 
-      function scrollTo(where) {
-        if(currentImage > 0) {
-          $("#previous").css("cursor", "w-resize")
-        } else {
-          $("#previous").css("cursor", "default")
-        }
+// HOMEPAGE
 
-        if(currentImage == numImages-2) {
-          $("#next").css("cursor", "default")
-        } else {
-          $("#next").css("cursor", "e-resize")
-        }
-
-        $("#container").animate({
-					scrollLeft: $(pageImages[where]).position().left
-				}, 500, function(){
-					//do something
-  			});
-      }
-
-      $("html").scroll(function(){
-        console.log(true);
-      });
-
+      // grid element rollover on homepage
       $(".grid")
       .mouseover(function(){
+        // if window size is above mobile breakpoint
+        // should fix this to detect for touch moreso
         if(viewportWidth > mobileBreak) {
+          // calls rotate function for .grid element
           $(this).rotate();
         }
       })
       .mouseout(function(){
+        // if window size is above mobile breakpoint
+        // should fix this to detect for touch moreso
         if(viewportWidth > mobileBreak) {
+          // stops the fadeIn and fadeOut actions
          $( this ).find( "img" ).stop( true, true ).fadeOut();
          $( this ).find( "img" ).stop( true, true ).fadeIn();
+         // hides the .secondary-thumb items
          $(this).children("img[class*='secondary']" ).hide();
        }
       });
-
-      $.fn.rotate = function(){
-        return this.each(function() {
-
-          /* Cache element's children */
-          var $children = $(this).children("img");
-
-          /* Current element to display */
-          var position = -1;
-
-          /* IIFE */
-          !function loop() {
-
-              /* Get next element's position.
-               * Restarting from first children after the last one.
-               */
-              position = (position + 1) % $children.length;
-
-              /* Fade element */
-              $children.eq(position).fadeIn(0).delay(300).fadeOut(0, loop);
-          }();
-        });
-      };
-
-
-      //KEEP
-      //click image to advance
-      /*
-			$(".row>img").click(function(){
-				$("#container").animate({
-					scrollLeft: $(this).position().left
-				}, 500, function(){
-					if($(this).position().left == containerLeft) {
-            console.log($(this))
-          }
-			});
-    });*/
-
-
-
-			/*$("#title").click(function(){
-				$("#container").animate({
-					scrollLeft: $("#title").width()+ 50
-				}, 500, function(){
-					console.log("done");
-			});
-			}); */
+    };
